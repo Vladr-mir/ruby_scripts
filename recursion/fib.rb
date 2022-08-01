dcc4abfcbcf8906c2d4eb23b97c5dab79bf5ada6
Vladimir <duenasvladimir@gmail.com>
2022-07-31 22:02:09 -0600

    Refactored the fibonacci solution

--- recursion/fib.rb
+++ recursion/fib.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: false
+
 def fibs(num)
   arr = [0, 1]
-  for i in 2.upto(num - 1)
-    arr << arr[i - 2] + arr[i - 1]
+  2.upto(num - 1).each do |i|
+    arr << (arr[i - 2] + arr[i - 1])
   end
   arr
 end
 
-def fibs_rec(num, i = 2, result = [0, 1])
-  return result if i >= num
-  result << result[i - 2] + result[i - 1]
+def fibs_rec(num, index = 2, result = [0, 1])
+  return result if index >= num
+
+  result << (result[index - 2] + result[index - 1])
   fibs_rec(num, i + 1, result)
 end
 
