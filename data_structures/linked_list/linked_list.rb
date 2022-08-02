d8b11fb420479c94911077f3df6a6bec50e27d2b
Vladimir <duenasvladimir@gmail.com>
2022-08-02 01:43:26 -0600

    Added more methods to the LinkedList class

--- data_structures/linked_list/linked_list.rb
+++ data_structures/linked_list/linked_list.rb
@@ -2,7 +2,7 @@
 
 # LinkedList implementation
 class LinkedList
-  attr_accessor :head
+  attr_reader :size, :head
 
   def initialize
     @head = nil
@@ -11,34 +11,43 @@ class LinkedList
 
   def append(value)
     @size += 1
+    return @head = Node.new(value, nil) if @head.nil?
 
-    if @head.nil?
-      @head = Node.new(value, nil)
-    else
-      last_node = @head
-      last_node = last_node.next_node until last_node.next_node.nil?
-      last_node.next_node = Node.new(value, nil)
-    end
+    last_node = @head
+    last_node = last_node.next_node until last_node.next_node.nil?
+    last_node.next_node = Node.new(value, nil)
   end
 
-  def prepend(value); end
+  def prepend(value)
+    @size += 1
+    return @head = Node.new(value, nil) if @head.nil?
+    @head = Node.new(value, @head)
+  end
 
-  def size; end
+  def tail
+    return @head = Node.new(value, nil) if @head.nil?
 
-  def tail; end
+    last_node = @head
+    last_node = last_node.next_node until last_node.next_node.nil?
+    last_node
+  end
 
   def at(index)
-    return Node.new(nil, nil) if index > @size
+    return Node.new(nil, nil) if index >= @size
 
     last_node = @head
     0.upto(index).each do |pos|
-      return last_node if pos == index - 1
-
+      return last_node if pos == index
       last_node = last_node.next_node
     end
   end
 
-  def pop; end
+  def pop
+    return @head = Node.new(value, nil) if @head.nil?
+
+    @size -= 1
+    self.at(size - 1).destroy_next
+  end
 end
 
 # Value node for LinkedList
@@ -48,5 +57,9 @@ class Node
   def initialize(value, next_node)
     @value = value
     @next_node = next_node
+  end
+
+  def destroy_next
+    @next_node = nil
   end
 end
