a50f5ca9b0a5956dbca23577c89df9cd5e12676a
Vladimir <duenasvladimir@gmail.com>
2022-08-04 23:52:26 -0600

    Refactoring of the code in the BinaryTree class

--- data_structures/binary_tree/binary_tree.rb
+++ data_structures/binary_tree/binary_tree.rb
@@ -47,9 +47,9 @@ class BinaryTree
     end
   end
 
-  def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+  def pretty_print(node = @root, prefix = '', is_left: true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true) if node.left
   end
 end
