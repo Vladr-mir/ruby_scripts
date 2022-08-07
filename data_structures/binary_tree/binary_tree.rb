9f8b4f3e2f3d20cb2f940e15ced4f7a04e5c5f3d
Vladimir <duenasvladimir@gmail.com>
2022-08-07 15:01:39 -0600

    Added balanced? method to BinaryTree class

--- data_structures/binary_tree/binary_tree.rb
+++ data_structures/binary_tree/binary_tree.rb
@@ -130,6 +130,16 @@ class BinaryTree
     [right_height, left_height].max + 1
   end
 
+  def balanced?
+    return true if @root.nil?
+    return true if @root.right.nil? && @root.left.nil?
+
+    right = height(@root.right)
+    left = height(@root.left)
+
+    right == left
+  end
+
   private
 
   def leftmost(node = @root)
